e16197193ed24cad921996fbb9f9894d9707e26d
Kashish <97381200+KshshVrma@users.noreply.github.com>
2022-09-11 11:13:29 +0530

    Time: 57 ms (5.68%), Space: 19.7 MB (14.25%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int max=0;
-        int maxcount=0;
-        if(nums.size()==1){
-            return nums[0];
+      unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
-      int  maxvar=INT_MIN;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                max++;
-                if(max>=maxcount){
-                    maxcount=max;
-                    maxvar=nums[i];
-                }
-            }
-            else{
-                max=0;
-            }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->second>nums.size()/2)
+            return it->first;
+        }
             
-            
-            
-}
-        return maxvar;
+return -1;
     }
 };
