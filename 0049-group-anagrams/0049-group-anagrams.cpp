7db0766bb576f1410470a75c3926c2615c0398dc
Kashish <97381200+KshshVrma@users.noreply.github.com>
2022-10-28 13:56:59 +0530

    Time: 101 ms (22.64%), Space: 23.7 MB (17.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
      vector<vector<string>> groupAnagrams(vector<string>& s) {
-        int n=s.size();
-        // maps sorted string to its anagrams in the array
-        unordered_map<string,vector<string>> m;
-        for(int i=0;i<n;i++){
-        // this string is used as a frequency counter of characters for each string 
-            string temp(26,'0');
-            for(char c:s[i]){
-                temp[c-'a']++;
-            }
-            // store in map
-            m[temp].push_back(s[i]);
-        }
-        vector<vector<string>> res;
-        for(auto i:m){
-            res.push_back(i.second);
-        }
-        return res;
+   int n=s.size();
+         unordered_map<string,vector<string>>mp;
+         for(int i=0;i<n;i++){
+             string temp(26,'0');
+             for(char c:s[i]){
+                 temp[c-97]++;
+             }
+             mp[temp].push_back(s[i]);
+         }
+         vector<vector<string>>ans;
+         for(auto it:mp){
+             ans.push_back(it.second);
+         }
+         return ans;
     }
 };
